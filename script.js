5c05d041e2ba4aab751478322206ad377ff6aeb3
Anquilis <43358398+Anquilis@users.noreply.github.com>
2019-05-13 15:47:01 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+$(".garage-door-opener").click(function() {
+    $(".garage-door").slideToggle();
+});
+
+$(".pokeball").click(function() {
+    $(".pikachu").fadeToggle();
+});
+
+$(".light-switch").click(function() {
+    $(".weak-light-bulb").toggle().fadeOut();
+});
+
+$(".hand-door").click(function() {
+    $(".door").slideToggle();
+});
